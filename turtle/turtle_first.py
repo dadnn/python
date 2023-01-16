8b32c87b8d90285120702a9fad522fb7badddca1
Dylan Norman <dylan@normansoftware.io>
2023-01-16 06:57:12 -0600

    added turtle star example from py docs

--- turtle/turtle_first.py
+++ turtle/turtle_first.py
@@ -1,6 +1,7 @@
 import turtle
 t = turtle.Turtle()
 
+# make square
 t.forward(100)
 t.right(90)
 t.forward(100)
@@ -8,6 +9,30 @@ t.right(90)
 t.forward(100)
 t.right(90)
 t.forward(100)
+
+# make roof
+t.goto(50,50)
+t.goto(100,0)
+
+# go to and make door
+t.goto(100,-100)
+t.goto(55,-100)
+t.goto(55,-80)
+t.goto(45,-80)
+t.goto(45,-100)
+
+# make path
+t.goto(0,-400)
+t.goto(34,-400)
+t.goto(55,-100)
+
+# make 3d
+t.goto(120,-100)
+t.goto(122,0)
+t.goto(100,0)
+t.goto(50,50)
+t.goto(75,50)
+t.goto(122,0)
 
 
 input("Press any key to exit ...")
